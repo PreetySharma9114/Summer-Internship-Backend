3a1bd612380552c9f8cacbbb080ca31bcc4a24d6
PreetySharma9114 <preetysharma8474@gmail.com>
2026-07-15 22:02:34 +0530

    Updated Instagram Code

--- src/modules/instagram/instagram.controller.ts
+++ src/modules/instagram/instagram.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { ApiResponse } from "../../shared/utils/apiResponse.js";
 import { InstagramExchangeDto } from "./instagram.dto.js";
 import { InstagramService } from "./instagram.service.js";
+import { ResponseUtil } from "../../shared/utils/response.util.js";
 
 const instagramService = new InstagramService();
 
@@ -14,7 +15,7 @@ export const InstagramController = {
     try {
       const result = await instagramService.exchange(req.body.code);
 
-      ApiResponse.success(res, result);
+      return ResponseUtil.success(res, result);
     } catch (error) {
       next(error);
     }
