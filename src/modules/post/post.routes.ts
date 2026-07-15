e14550b370947363557dbf87e877051e54f88647
PreetySharma9114 <preetysharma8474@gmail.com>
2026-07-16 02:14:55 +0530

    Added auth middleware in post routes

--- src/modules/post/post.routes.ts
+++ src/modules/post/post.routes.ts
@@ -7,8 +7,11 @@ import {
 } from "./post.dto.js";
 import { validate } from "../../shared/middlewares/validation.middleware.js";
 import { upload } from "../../shared/middlewares/upload.middleware.js";
+import { authenticate } from "../../shared/middlewares/auth.middleware.js";
 
 export const PostRouter = Router();
+
+PostRouter.use(authenticate);
 
 PostRouter.post(
   "/caption",
